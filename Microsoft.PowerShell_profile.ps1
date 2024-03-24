4627238181c28f90193166daec4a10b2699519c1
Kasper Wind <kasperwind79@gmail.com>
2024-03-24 10:51:18 +0100

    Updated to work with alt+c

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
+# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r' -TabExpansion
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Set-Alias -Name cdi -Value zi
 Set-Alias -Name cd -Value z -Option AllScope
 
@@ -13,9 +16,8 @@ function lt {eza --long --tree}
 
 # default ripgrep
 # rg --files --hidden --glob '!*.git' --glob '!AppData' --glob '!.nuget' --glob '!.dotnet'
-
-$Env:FZF_ALT_C_OPTS="-walker-skip .git,node_modules,target,obj --preview 'tree -C {} | head -200'"
-# New-Item -Path Env:\FZF_ALT_C_OPTS -Value "-walker-skip .git,node_modules,target,obj --preview 'tree -C {} | head -200'"
+$Env:FZF_ALT_C_OPTS='--walker-skip .git,node_modules,target,obj,AppDate,.nuget --preview "tree {}"'
 
 function fzf_preview { cmd /c fzf --preview "bat --style=numbers --color=always --line-range :500 {}" }
 Set-Alias -Name fzf -Value fzf_preview
+
--- README.md
+++ README.md
@@ -13,6 +13,7 @@ winget install BurntSushi.ripgrep.MSVC
 winget install fzf
 winget install --id Starship.Starship
 winget install ajeetdsouza.zoxide
+Install-Module -Name PSFzf
 ```
 
 # Setup
@@ -21,5 +22,5 @@ winget install ajeetdsouza.zoxide
 
 ```
 FZF_DEFAULT_COMMAND = rg --files --hidden --glob "!*.git" --glob "!AppData" --glob "!.nuget" --glob "!.dotnet"  
-FZF_ALT_C_OPTS = --walker-skip .git,node_modules,target,obj,AppDate,.nuget --preview "tree -C {} | head -200"
+FZF_ALT_C_OPTS =--walker-skip .git,node_modules,target,obj,AppDate,.nuget --preview "tree -C {}"
 ```
